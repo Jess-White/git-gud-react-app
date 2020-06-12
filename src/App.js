16d6470c3e0be805874470ce019ed67fd6b4af5f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-06-12 12:27:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// import SignUp from './components/SignUp';
+// import LogIn from './components/LogIn';
+// import Dashboard from './components/Dashboard';
+// import Library from './components/Library';
+// import ResourceNew from './components/ResourceNew';
+// import ReviewNew from './components/ReviewNew';
+// import ReviewShow from './components/ReviewShow';
+// import ResourceShow from './components/ResourceShow';
+// import ResourceEdit from './components/ResourceEdit';
+// import ReviewEdit from './components/ReviewEdit';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    function App() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
+function Header() {
+  return (
+          <div>
+            <Jumbotron fluid style={{backgroundColor: "#00008b"}}>
+              <Container fluid>
+                <div className="row">
+                  <img style={{margin: 20, marginLeft: 200}} src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F012%2F982%2F039.jpg" alt=""/>
+                  <h1 style={{
+                    marginLeft: 80, 
+                    marginTop: 200, 
+                    color: "#39ff14", 
+                    fontWeight: "bold",
+                    fontFamily: "courier",
+                    fontStretch: "expanded",
+                    fontSize: 150
+                  }}className="display-1">Git Gud</h1>
+                  <h2 style={{
+                    margin: "auto", 
+                    color: "#39ff14", 
+                    fontWeight: "bold",
+                    fontFamily: "courier",
+                    fontStretch: "expanded",
+                    fontSize: 50
+                  }}>need slogan....</h2>
+                </div>
+              </Container>
+            </Jumbotron>
+          </div>
+    )
+}
+
+export default Header
